4ba171dc9c326bf1a83cca13e37f7e1fa1b3834f
Opilane TTHK <opilane@tthk.ee>
2021-10-27 11:33:02 +0300

    first commit

---
+++ FileCreayte/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+
+namespace FileCreayte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootDirectory = @"c:\Users/opilane/samples";
+            string userFile = "wishList";
+            string FullFilePath = $@"{rootDirectory} / {userFile}.txt";
+            File.Create(FullFilePath);
+
+            bool directoryExists = Directory.Exists(rootDirectory);
+            bool fileExists = File.Exists(FullFilePath);
+            
+            if (directoryExists && fileExists)
+            {
+                Console.WriteLine($"{userFile} already exists in {rootDirectory}");
+
+            }
+            else if (!directoryExists)
+            {
+                Console.WriteLine($"{rootDirectory} does not exist.");
+                Directory.CreateDirectory(rootDirectory);
+                Console.WriteLine($"{rootDirectory} has been created");
+                File.Create(FullFilePath);
+                Console.WriteLine($"{userFile}.txt has been created in {rootDirectory}");
+            }
+            else
+            {
+                File.Create(FullFilePath);
+                Console.WriteLine($"{userFile}.txt has been created in {rootDirectory}");
+            }
+            
+
+        }
+    }
+}
